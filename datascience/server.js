df98081b3637df9e42b52d879a21713f15df375f
Santiago Cotto <scotto@goshops.ai>
2025-05-11 21:08:24 -0300

    asd

--- datascience/server.js
+++ datascience/server.js
@@ -62,16 +62,16 @@ app.get('/', (req, res) => {
       const modifiedDate = file.modified.toLocaleString();
       
       return `
-        <tr class="border-b border-gray-200 hover:bg-gray-100">
+        <tr class="hover:bg-gray-700">
           <td class="py-3 px-4">
-            <a href="${jupyterUrl}" target="_blank" class="text-blue-600 hover:underline flex items-center">
+            <a href="${jupyterUrl}" target="_blank" class="text-blue-400 hover:text-blue-300 flex items-center">
               <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
               </svg>
               ${file.name}
             </a>
           </td>
-          <td class="py-3 px-4 text-gray-500">${modifiedDate}</td>
+          <td class="py-3 px-4 text-gray-400">${modifiedDate}</td>
         </tr>
       `;
     })
